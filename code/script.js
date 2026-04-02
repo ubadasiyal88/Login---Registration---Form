b56c5c24bb8837f64e77e9778dfcae7facdd428b
kiranShamsHere <kiranshamsdhiloo@gmail.com>
2026-04-02 10:52:33 +0500

    Added show hide password feature

--- code/script.js
+++ code/script.js
@@ -9,3 +9,22 @@ registerBtn.addEventListener('click',()=>{
 loginBtn.addEventListener('click',()=>{
     container.classList.remove('active');
 })
+
+// Toggle password visibility: By Kiran Shams
+const togglePasswords = document.querySelectorAll('.toggle-password');
+
+togglePasswords.forEach(icon => {
+    icon.addEventListener('click', () => {
+        const input = icon.previousElementSibling;
+
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.remove('fa-lock');
+            icon.classList.add('fa-eye');
+        } else {
+            input.type = 'password';
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-lock');
+        }
+    });
+});
